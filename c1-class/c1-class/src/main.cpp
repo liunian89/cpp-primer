6433ed8ce731b2af3681b9fa142150abe682e12b
Nian Liu <nliu@bol.com>
2018-03-29 23:17:28 +0200

    chapter 1 class total

--- c1-class/c1-class/src/main.cpp
+++ c1-class/c1-class/src/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include "../include/Sales_item.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    Sales_item item1, item2;
+    Sales_item total;
     
-    std::cin >> item1 >> item2;
-    
-    if (item1.isbn() == item2.isbn()) {
-        std::cout << item1 + item2 << std::endl;
-        return 0;   // indicate success
+    if (cin >> total) {
+        Sales_item next;
+        
+        while (cin >> next) {
+            if (total.isbn() == next.isbn()) {
+                total += next;
+            } else {
+                cout << "Total is: " << total << endl;
+                total = next;
+            }
+        }
+        cout << "Total is: " << total << endl;
+        
     } else {
-        std::cerr << "Data must refer to same ISBN"
-        << std::endl;
+        // no input! warn the user
+        std::cerr << "No data?!" << std::endl;
         return -1;  // indicate failure
     }
+    
+    return 0;
 }
